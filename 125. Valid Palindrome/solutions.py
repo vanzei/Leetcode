d2fb524202ecfc50eb4582d53eed2c2bd6a6396b
vanzei <leonardo.vanzei123@gmail.com>
2023-06-28 22:58:38 -0400

    Valid Palindrome

---
+++ 125. Valid Palindrome/solutions.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = ''.join(filter(str.isalnum, s)).lower()
+        print(s)
+        i, j = 0, len(s)-1
+        while i < j:
+            if s[i] == s[j]:
+                i += 1
+                j -= 1
+            else:
+                return False
+        return True
