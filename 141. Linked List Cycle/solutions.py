77a612e41299c47011ad45601889c656386d54ca
vanzei <leonardo.vanzei123@gmail.com>
2023-08-12 15:01:46 -0400

    141

---
+++ 141. Linked List Cycle/solutions.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        first, second = head, head
+        while second is not None and second.next is not None:
+            first = first.next
+            second = second.next.next
+            if first == second:
+                return True
+        return False
+
