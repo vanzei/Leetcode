ada6630aaf21d2fa796907f2dc0f891df8943984
vanzei <leonardo.vanzei123@gmail.com>
2023-10-15 20:31:01 -0400

    oct-15

---
+++ python/124. Binary Tree Maximum Path Sum/solution.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        res = [root.val]
+        
+        def dfs(root):
+            if not root:
+                return 0
+
+            maxL = dfs(root.left)
+            maxR = dfs(root.right)
+            maxL = max(0, maxL)
+            maxR = max(0, maxR)
+
+            res[0] = max(res[0], root.val + maxL + maxR)
+
+            return root.val + maxL + maxR
+        
+        dfs(root)
+        return res[0]
